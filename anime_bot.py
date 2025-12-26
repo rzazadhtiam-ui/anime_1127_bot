8fd350c11311094715ab77b2c65ca17715d9aeb9
tiam <rzazadhtiam@gmail.com>
2025-12-26 17:15:36 +0330

    Update anime_bot.py

--- anime_bot.py
+++ anime_bot.py
@@ -203,7 +203,7 @@ if __name__ == "__main__":
     import os
 
     def run_flask():
-        app.run(host="0.0.0.0", port=8080)
+        app.run(host="0.0.0.0", port=port)
 
     threading.Thread(target=run_flask, daemon=True).start()
 
