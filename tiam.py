4bd07a8a15f2b944f6fb6c33cbfd8e3b8e8916c0
tiam <rzazadhtiam@gmail.com>
2026-02-05 18:05:59 +0330

    Update tiam.py

--- tiam.py
+++ tiam.py
@@ -140,7 +140,22 @@ async def handler(event):
 
     except Exception as e:
         await edit_status(msg, link, str(e), 0)
+#===============================================
+from flask import Flask
+import threading
+import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
 
 # ================= MAIN =================
 
