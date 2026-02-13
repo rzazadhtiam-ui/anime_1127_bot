9a9c096360fb4aa78d5daf835bddb4887d08af6d
tiam <rzazadhtiam@gmail.com>
2026-02-13 17:53:17 +0330

    Update mine.py

--- self_bot/mine.py
+++ self_bot/mine.py
@@ -1,12 +1,33 @@
 import telebot
 from update1 import PanelManager
+from flask import Flask
+import threading
+import os
 
 TOKEN = "8550709057:AAFzGO1-sCzxIHqJ0raZkB1yg9AqeO1PrJU"
-bot = telebot.TeleBot(TOKEN)
 
-# ساخت پنل منیجر
+bot = telebot.TeleBot(TOKEN)
 panel = PanelManager(bot)
 
-# حالا میتونی infinity_polling بزنی
-print("Bot Running...")
-bot.infinity_polling(timeout=30, long_polling_timeout=30)
+# -------- Flask Server --------
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive ✅"
+
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
+# -------- Telegram Bot --------
+def run_bot():
+    print("Bot Running...")
+    bot.infinity_polling(timeout=30, long_polling_timeout=30)
+
+
+# -------- Threads --------
+threading.Thread(target=run_web).start()
+threading.Thread(target=run_bot).start()
