eccf1b3fc065435a87ca7bdeba78eaaa0d30db9d
tiam <rzazadhtiam@gmail.com>
2026-05-31 13:56:45 +0330

    Update anime_bot.py

--- anime_bot.py
+++ anime_bot.py
@@ -929,14 +929,12 @@ def home():
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    try:
-        json_str = request.get_data().decode("utf-8")
-        update = telebot.types.Update.de_json(json_str)
+    if request.headers.get("content-type") == "application/json":
+        json_string = request.get_data().decode("utf-8")
+        update = Update.de_json(json_string)
         bot.process_new_updates([update])
         return "OK", 200
-    except Exception as e:
-        print("WEBHOOK ERROR:", e)
-        return "ERROR", 200
+    return "bad request", 403
 
 # =======================
 if __name__ == "__main__":
